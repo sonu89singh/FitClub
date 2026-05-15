a171db8bc0022a54b911058e31c52c78ed30b971
ashish champia <ashishchampia150@gmail.com>
2026-05-15 20:08:47 +0530

    request respos

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express()
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("hello from node js server");
+});
+
+app.get("/about", (req, res) => {
+   res.send("this is for about ");
+});
+
+app.post("/register", (req, res) => {
+    console.log(req.body.name);
+res.send(req.body.name + "has registered with" + req.body.email);
 });
 
 app.listen(3000, () => {
