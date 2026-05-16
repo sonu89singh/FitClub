bcabcacbd5e5b3b1a7b1b7ceb9db78d6f7317e8e
ashish champia <ashishchampia150@gmail.com>
2026-05-16 13:27:53 +0530

    connection done with db and checked if post is working correctly ad add data using .query.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express()
 app.use(express.json());
 
@@ -13,8 +14,30 @@ app.get("/about", (req, res) => {
 app.post("/register", (req, res) => {
     console.log(req.body.name);
 res.send(req.body.name + "has registered with" + req.body.email);
+    connection.query("INSERT INTO users (name, email) VALUES (?,?)", [req.body.name, req.body.email], (err , result) => {
+        if (err) {
+            console.log("not saved", err);
+            return;
+        }
+        console.log("saved sucssesfully");
+    });
 });
 
 app.listen(3000, () => {
     console.log("server ruiig o port 3000");
+});
+
+const connection = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "!Q@W1q2w",
+    database : "fitflow"
+});
+ 
+connection.connect((err) => {
+   if(err) {
+    console.log("db is not connected ");
+    return;
+   } 
+    console.log("db is running...");
 });
